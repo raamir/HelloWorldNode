ed92e5862f193af4ec194539d44a7119a70f0d55
raamir <raamir@mail.depaul.edu>
2019-09-20 22:17:42 -0400

    Node JS Server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var port = 3000;
+var host = "0.0.0.0";
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
+})
